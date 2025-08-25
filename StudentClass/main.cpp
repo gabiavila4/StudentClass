6aaed2acd39f49e7f09845368c8b17e6711cca0d
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-08-25 10:19:14 -0500

    Initial Commit

---
+++ StudentClass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StudentClass
+//
+//  Created by Gabriela Avila on 8/25/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
